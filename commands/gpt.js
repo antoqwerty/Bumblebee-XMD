40782de19398a2b42f744a9ceefd6cd05cb0b7a3
Bright_Leizer📌 <blacktappy5@gmail.com>
2025-03-07 20:40:15 +0300

    𝘽𝙪𝙢𝙗𝙡𝙚𝙗𝙚𝙚-𝙓𝙈𝘿

---
+++ commands/gpt.js
@@ -0,0 +1,30 @@
+const { ezra } = require("../fredi/ezra");
+const axios = require("axios");
+
+// C'est la commande pour interagir avec ChatGPT thomas
+ezra({ nomCom: "gpt3", reaction: "🌐", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+
+    try {
+        // Vérifie si des arguments ont été fournis
+        if (!arg || arg.length === 0) {
+            return repondre("Veuillez poser une question.");
+        }
+
+        // Regrouper les arguments en une seule chaîne ecrit  par thomas
+        const question = arg.join(' ');
+
+        // Appel à l'API ChatGPT avec la nouvelle URL recherche par thomas
+        const responseApi = await axios.get(`https://test-api-apms.onrender.com/api/chatgpt?text=${encodeURIComponent(question)}&name=Kaizoku&prompt=${encodeURIComponent("Tu seras une IA d'un bot WhatsApp tres puissant du nom ✧⁠𝘽𝙪𝙢𝙗𝙡𝙚𝙗𝙚𝙚-𝙓𝙈𝘿✧")}&apikey=BrunoSobrino`);
+
+        const resultat = responseApi.data;
+        if (resultat) {
+            repondre(resultat.resultado);
+        } else {
+            repondre("Erreur lors de la génération de la réponse.");
+        }
+    } catch (error) {
+        console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+        repondre("Oups, une erreur est survenue lors du traitement de votre demande.");
+    }
+});
