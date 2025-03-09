c71c804322eaec1856dc4115975f6c287098d36d
Bright_Leizer📌 <blacktappy5@gmail.com>
2025-03-09 18:56:50 +0300

    𝘽𝙪𝙢𝙗𝙡𝙚𝙗𝙚𝙚-𝙓𝙈𝘿

---
+++ framework/bright.js
@@ -0,0 +1,17 @@
+
+var tabCmds = [];
+let cm = [];
+function bright(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🤦";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { bright, Module: bright, cm };
