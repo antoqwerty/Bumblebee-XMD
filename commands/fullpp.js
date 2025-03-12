226426e9f47a41a861fe61851e334fc572a59dca
Bright_Leizer📌 <blacktappy5@gmail.com>
2025-03-12 11:18:12 +0300

    𝘽𝙪𝙢𝙗𝙡𝙚𝙗𝙚𝙚-𝙓𝙈𝘿

--- commands/fullpp.js
+++ commands/fullpp.js
@@ -14,7 +14,7 @@ const s = require(__dirname + "/../set");
 
 
 
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "menu2", categorie: "General" }, async (dest, zk, commandeOptions) => {
 
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
 
